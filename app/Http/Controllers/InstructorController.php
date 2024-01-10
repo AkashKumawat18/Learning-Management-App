1a71404931563f0682705c2edc669442c47eae9b
Akash Kumawat <kashu7102@gmail.com>
2024-01-10 11:32:53 +0530

    setup instructor logout option

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class InstructorController extends Controller
 {
     public function InstructorDashboard(){
         return view("instructor.index");
-    }
+    }//End Method
+
+    public function InstructorLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/instructor/login');
+    }//End Method
+
+    public function InstructorLogin(){
+        return view("instructor.instructor_login");
+    }// End Method
 }
--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,13 @@ Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.
 
 //Instructor group middleware
 Route::middleware(['auth','roles:instructor'])->group(function () {
-    Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructr.dashboard');
-}); ///End instructor group middleware
+    Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+    Route::get('/instructor/logout', [InstructorController::class, 'InstructorLogout'])->name('instructor.logout');
+}); 
+
+Route::get('/instructor/login', [InstructorController::class, 'InstructorLogin'])->name('instructor.login');
+
+///End instructor group middleware
 
 
 
