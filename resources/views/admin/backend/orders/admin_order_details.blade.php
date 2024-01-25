cb063597931bc961be4abbbe38dadb02c334a8c9
Akash Kumawat <kashu7102@gmail.com>
2024-01-25 14:00:44 +0530

    Admin Dashboard Order Display Part 3

--- resources/views/admin/backend/orders/admin_order_details.blade.php
+++ resources/views/admin/backend/orders/admin_order_details.blade.php
@@ -1,7 +1,6 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-
 <div class="page-content">
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
@@ -16,7 +15,7 @@
             </nav>
         </div>
         <div class="ms-auto">
-
+           
         </div>
     </div>
     <!--end breadcrumb-->
@@ -24,80 +23,187 @@
         <div class="main-body">
             <div class="row">
                 <div class="col-lg-6">
-
-                    <div class="card">
-
-                        <form method="POST" action="{{ route('admin.profile.store') }}"  enctype="multipart/form-data">
-                            @csrf
-
+                    <div class="card"> 
+                
                         <div class="card-body">
                             <div class="row mb-3">
                                 <div class="col-sm-3">
                                     <h6 class="mb-0">Name</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    ddddddddddd
+                                    <strong> {{ $payment->name }}</strong> 
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <div class="col-sm-3">
-                                    <h6 class="mb-0">User Name</h6>
+                                    <h6 class="mb-0">Email</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                   fffffffffffffffffff
+                                    <strong>  {{ $payment->email }}</strong> 
                                 </div>
                             </div>
-
-
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Phone</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <strong>  {{ $payment->phone }}</strong> 
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Address</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <strong>  {{ $payment->address }}</strong> 
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Payment Type </h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <strong>  {{ $payment->cash_delivery }}</strong> 
+                                </div>
+                            </div>
+                               
+                
                         </div> 
-                    </form>
-
-
+                     
+                
+                
                     </div>
-
+                 
                 </div>
-
-
 <div class="col-lg-6">
     <div class="card">
-
-        <form method="POST" action="{{ route('admin.profile.store') }}"  enctype="multipart/form-data">
-            @csrf
-
+ 
         <div class="card-body">
+            
             <div class="row mb-3">
                 <div class="col-sm-3">
-                    <h6 class="mb-0">Name</h6>
+                    <h6 class="mb-0">Total Amount  </h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                    ddddddddddd
+                    <strong> ${{ $payment->total_amount }}</strong> 
                 </div>
             </div>
             <div class="row mb-3">
                 <div class="col-sm-3">
-                    <h6 class="mb-0">User Name</h6>
+                    <h6 class="mb-0">Payment Type </h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                   fffffffffffffffffff
+                 <strong>{{ $payment->payment_type }}</strong>   
                 </div>
             </div>
-
-
-
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Invoice Number </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <strong> {{ $payment->invoice_no }}</strong> 
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Order Date </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <strong>  {{ $payment->order_date }}</strong> 
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Status  </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    @if ($payment->status == 'pending')
+                    <a href="" class="btn btn-block btn-success">Confirm Order</a>
+                    @elseif ($payment->status == 'confirm')    
+                    <a href="" class="btn btn-block btn-success">Confirm Order</a>
+                    @endif 
+                     
+                </div>
+            </div>
         </div> 
-    </form>
-
-
+     
     </div>
-
-
-
+    
 </div>
             </div>
+<div class="card radius-10">
+    <div class="card-body">
+        <div class="d-flex align-items-center">
+            
+         <div class="flex-grow-1 ms-3">
+            <div class="table-responsive">
+                <table class="table" style="font-weight: 600;">
+                    <tbody>
+                        <tr>
+                            <td class="col-md-1">
+                                <label>Image</label>
+                            </td>
+                            <td class="col-md-2">
+                                <label>Course Name</label>
+                            </td>
+                            <td class="col-md-2">
+                                <label>Category </label>
+                            </td>
+                            <td class="col-md-2">
+                                <label>Instructor</label>
+                            </td>
+                            <td class="col-md-2">
+                                <label>Price</label>
+                            </td> 
+                        </tr>
+@php
+    $totalPrice = 0;
+@endphp
+                        @foreach ($orderItem as $item)
+                        <tr>
+                            <td class="col-md-1">
+                                <label><img src="{{ asset($item->course->course_image) }}" alt="" style="width: 50px; height:50px;"> </label>
+                            </td>
+                            <td class="col-md-2">
+                                <label> {{ $item->course->course_name }}  </label>
+                            </td>
+                            <td class="col-md-2">
+                                <label>{{ $item->course->category->category_name }}</label>
+                            </td>
+                            <td class="col-md-2">
+                                <label> {{ $item->instructor->name }} </label>
+                            </td>
+                            <td class="col-md-2">
+                                <label> ${{ $item->price }} </label>
+                            </td>
+                        </tr>
+
+             @php
+                 $totalPrice += $item->price; 
+             @endphp           
+
+                        @endforeach
+
+             <tr>
+                <td colspan="4"></td>
+                <td class="col-md-3">
+                <strong>Total Price : ${{ $totalPrice }}</strong>    
+                </td>    
+            </tr>             
+
+
+                    </tbody>
+
+                </table>
+                
+            </div>
+            </div>   
+            
         </div>
     </div>
 </div>
-
-
-
+        </div>
+    </div>
+</div>
+ 
 @endsection
