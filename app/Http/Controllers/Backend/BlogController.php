4e560e8c00ee7274e476c86ebbac747c1099f3bf
Akash Kumawat <kashu7102@gmail.com>
2024-01-26 14:31:02 +0530

    Blog Category Setup Part 2

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -14,4 +14,20 @@ class BlogController extends Controller
         return view('admin.backend.blogcategory.blog_category',compact('category'));
     
        }// End Method 
+
+       public function StoreBlogCategory(Request $request){
+
+        BlogCategory::insert([
+            'category_name' => $request->category_name,
+            'category_slug' => strtolower(str_replace(' ','-',$request->category_name)),
+        ]);
+    
+        $notification = array(
+            'message' => 'BlogCategory Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    
+    
+       }// End Method 
 }
