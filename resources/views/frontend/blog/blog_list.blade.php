ab5c7ca8d753314e36811d6870a6a7182c6be6fa
Akash Kumawat <kashu7102@gmail.com>
2024-01-26 16:22:50 +0530

    Custom Pagination Part 1

--- resources/views/frontend/blog/blog_list.blade.php
+++ resources/views/frontend/blog/blog_list.blade.php
@@ -1,6 +1,5 @@
 @extends('frontend.master')
 @section('home')
-
 <!-- ================================
     START BREADCRUMB AREA
 ================================= -->
@@ -14,7 +13,7 @@
             <ul class="generic-list-item generic-list-item-white generic-list-item-arrow d-flex flex-wrap align-items-center">
                 <li><a href="index.html">Home</a></li>
                 <li>Blog</li>
-
+                
             </ul>
         </div><!-- end breadcrumb-content -->
     </div><!-- end container -->
@@ -22,7 +21,6 @@
 <!-- ================================
     END BREADCRUMB AREA
 ================================= -->
-
 <!-- ================================
        START BLOG AREA
 ================================= -->
@@ -31,8 +29,8 @@
         <div class="row">
             <div class="col-lg-8 mb-5">
                 <div class="row">
-
-
+                  
+                  
    @foreach ($blog as $item)
     <div class="col-lg-6">
         <div class="card card-item">
@@ -66,36 +64,22 @@
         </div><!-- end card -->
     </div><!-- end col-lg-6 --> 
     @endforeach       
-
-
-
+                     
                 </div><!-- end row -->
                 <div class="text-center pt-3">
                     <nav aria-label="Page navigation example" class="pagination-box">
-                        <ul class="pagination justify-content-center">
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Previous">
-                                    <span aria-hidden="true"><i class="la la-arrow-left"></i></span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Next">
-                                    <span aria-hidden="true"><i class="la la-arrow-right"></i></span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </li>
-                        </ul>
+
+                  {{-- {{ $blog->links() }} --}}
+
+                  {{ $blog->links('vendor.pagination.custom') }}
+
                     </nav>
-                    <p class="fs-14 pt-2">Showing 1-10 of 56 results</p>
+
                 </div>
             </div><!-- end col-lg-8 -->
             <div class="col-lg-4">
                 <div class="sidebar">
-
+                     
                     <div class="card card-item">
                         <div class="card-body">
                             <h3 class="card-title fs-18 pb-2">Blog Category</h3>
@@ -103,9 +87,9 @@
                             <ul class="generic-list-item">
                                 @foreach ($bcategory as $cat)
                                 <li><a href="{{ url('blog/cat/list/'.$cat->id) }}">{{ $cat->category_name }}</a></li>
-
+                                   
                                 @endforeach
-
+                                
                             </ul>
                         </div>
                     </div><!-- end card -->
@@ -113,7 +97,7 @@
                         <div class="card-body">
                             <h3 class="card-title fs-18 pb-2">Recent Posts</h3>
                             <div class="divider"><span></span></div>
-
+                           
                            @foreach ($post as $dpost)
                             <div class="media media-card border-bottom border-bottom-gray pb-4 mb-4">
                                 <a href="{{ url('blog/details/'.$dpost->post_slug) }}" class="media-img">
@@ -124,16 +108,15 @@
                                     <span class="d-block lh-18 py-1 fs-14">Admin </span> 
                                 </div>
                             </div><!-- end media --> 
-
+                               
                             @endforeach
-
                             <div class="view-all-course-btn-box">
                                 <a href="blog-no-sidebar.html" class="btn theme-btn w-100">View All Posts <i class="la la-arrow-right icon ml-1"></i></a>
                             </div>
                         </div>
                     </div><!-- end card -->
-
-
+                   
+                  
                     <div class="card card-item">
                         <div class="card-body">
                             <h3 class="card-title fs-18 pb-2">Sidebar Form</h3>
@@ -156,7 +139,7 @@
                             </form>
                         </div>
                     </div><!-- end card -->
-
+                   
                     <div class="card card-item">
                         <div class="card-body">
                             <h3 class="card-title fs-18 pb-2">Connect & Follow</h3>
@@ -177,9 +160,4 @@
 <!-- ================================
        START BLOG AREA
 ================================= -->
-
-
-
-
-
 @endsection
